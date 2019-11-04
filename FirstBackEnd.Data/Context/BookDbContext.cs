4121fd18588df852fff8dc4a9e88eac16fa521a8
Saeed mohammadi <saeed.mohammadi@meditechsys.com>
2019-11-04 16:12:39 +0330

    create projects

---
+++ FirstBackEnd.Data/Context/BookDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBackEnd.Data.Context
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
